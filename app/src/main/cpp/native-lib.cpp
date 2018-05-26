f4fc85e92181308b4a09c5b6a378bb209ca28139
Ipsedo <7268@samu>
2018-05-26 15:39:46 +0200

    fait wrappers

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,8 @@
 #include <android/asset_manager_jni.h>
 
 #include "utils/assets.h"
+#include "level/level.h"
+#include <glm/gtc/type_ptr.hpp>
 
 extern "C" JNIEXPORT jstring
 
@@ -49,4 +51,74 @@ Java_com_samuelberrien_phyvr_MainActivity_test(JNIEnv *env, jobject instance, jo
 
     std::string objTxt = getFileText(cppMgr, "obj/icosahedron.obj");
     return env->NewStringUTF(objTxt.c_str());
+}
+
+/**
+ * Level stuff
+ */
+
+float* jfloatPtrToCppFloatPtr(jfloat* array, int length) {
+    float* res = new float[length];
+    for (int i = 0; i < length; i++) {
+        res[i] = array[i];
+    }
+    // TODO free this array
+    return res;
+}
+
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_com_samuelberrien_phyvr_MyGvrView_initLevel(JNIEnv *env, jobject instance,
+                                                 jobject assetManager) {
+    AAssetManager* cppMgr = AAssetManager_fromJava(env, assetManager);
+    Level* level = new Level(cppMgr);
+    level->init();
+    return (long) level;
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_samuelberrien_phyvr_MyGvrView_updateLevel(JNIEnv *env, jobject instance, jlong levelptr,
+                                                   jfloatArray mHeadView_) {
+    jfloat *mHeadView = env->GetFloatArrayElements(mHeadView_, NULL);
+    float *headView = jfloatPtrToCppFloatPtr(mHeadView, 16);
+
+    Level* level = (Level*) levelptr;
+    level->update(glm::make_mat4(headView));
+
+    env->ReleaseFloatArrayElements(mHeadView_, mHeadView, 0);
+    delete headView;
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_samuelberrien_phyvr_MyGvrView_drawLevel(JNIEnv *env, jobject instance, jlong levelptr,
+                                                 jfloatArray mEyeProjectionMatrix_,
+                                                 jfloatArray mEyeViewMatrix_,
+                                                 jfloatArray myLighPosInEyeSpace_,
+                                                 jfloatArray mCameraPos_) {
+    jfloat *mEyeProjectionMatrix = env->GetFloatArrayElements(mEyeProjectionMatrix_, NULL);
+    jfloat *mEyeViewMatrix = env->GetFloatArrayElements(mEyeViewMatrix_, NULL);
+    jfloat *myLighPosInEyeSpace = env->GetFloatArrayElements(myLighPosInEyeSpace_, NULL);
+    jfloat *mCameraPos = env->GetFloatArrayElements(mCameraPos_, NULL);
+
+    float* eyeProjectionMatrix = jfloatPtrToCppFloatPtr(mEyeProjectionMatrix, 16);
+    float* eyeViewMatrix = jfloatPtrToCppFloatPtr(mEyeViewMatrix, 16);
+    float* lighPosInEyeSpace = jfloatPtrToCppFloatPtr(myLighPosInEyeSpace, 4);
+    float* cameraPos = jfloatPtrToCppFloatPtr(mCameraPos, 3);
+
+    Level* level = (Level*) levelptr;
+    level->draw(glm::make_mat4(eyeProjectionMatrix),
+                glm::make_mat4(eyeViewMatrix),
+                glm::make_vec4(lighPosInEyeSpace),
+                glm::make_vec3(cameraPos));
+
+    env->ReleaseFloatArrayElements(mEyeProjectionMatrix_, mEyeProjectionMatrix, 0);
+    env->ReleaseFloatArrayElements(mEyeViewMatrix_, mEyeViewMatrix, 0);
+    env->ReleaseFloatArrayElements(myLighPosInEyeSpace_, myLighPosInEyeSpace, 0);
+    env->ReleaseFloatArrayElements(mCameraPos_, mCameraPos, 0);
+    delete eyeProjectionMatrix;
+    delete eyeViewMatrix;
+    delete lighPosInEyeSpace;
+    delete cameraPos;
 }

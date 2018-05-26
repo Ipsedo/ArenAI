65456999c32fe1eb4c3d0b3c3e9861773de7f13e
Ipsedo <7268@samu>
2018-05-26 15:39:19 +0200

    add compile shader

--- app/src/main/cpp/utils/shader.cpp
+++ app/src/main/cpp/utils/shader.cpp
@@ -1,4 +1,5 @@
 #include <GLES2/gl2.h>
+#include <android/log.h>
 #include "shader.h"
 
 GLuint loadShader(GLenum type, const char* shaderSource) {
@@ -6,6 +7,12 @@ GLuint loadShader(GLenum type, const char* shaderSource) {
     GLuint shader = glCreateShader(type);
 
     glShaderSource( shader, 1, &shaderSource, NULL );
+    glCompileShader(shader);
 
+
+    /*GLint success = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+    __android_log_print(ANDROID_LOG_INFO, "POIR", "POIR %s", success == 0 ? "pb" : "good");*/
     return shader;
 }

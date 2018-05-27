c7659f4ffc16c58ae900728ed7f5f09b1a8395d8
Ipsedo <7268@samu>
2018-05-27 22:05:08 +0200

    faire heightmap

---
+++ app/src/main/cpp/graphics/misc.h
@@ -0,0 +1,12 @@
+//
+// Created by samuel on 27/05/18.
+//
+
+#ifndef PHYVR_MISC_H
+#define PHYVR_MISC_H
+
+float* packedVertNorm(float* heightmap, int width, int height);
+
+float* packedNoiseMap(int width, int height);
+
+#endif //PHYVR_MISC_H
---
+++ app/src/main/java/com/samuelberrien/phyvr/utils/LoadImage.java
@@ -0,0 +1,60 @@
+package com.samuelberrien.phyvr.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LoadImage {
+
+	private boolean imageLoaded;
+	private Bitmap bitmap;
+
+	public LoadImage(Context context, String imgAssetPath) {
+		try {
+			InputStream in = context.getAssets().open(imgAssetPath);
+			BitmapFactory.Options options = new BitmapFactory.Options();
+			options.inSampleSize = 8;
+			bitmap = BitmapFactory.decodeStream(in, null, options);
+			imageLoaded = true;
+		} catch (IOException ioe) {
+			imageLoaded = false;
+			ioe.printStackTrace();
+		}
+	}
+
+	private float[] tofloatRGBArray() {
+		if (!imageLoaded) return new float[0];
+
+		int width = getWidth();
+		int height = getHeight();
+
+		float[] res = new float[width * height * 3];
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				res[i * width + j] = bitmap.getPixel(j, i);
+			}
+		}
+		return res;
+	}
+
+	public int getWidth() {
+		return bitmap.getWidth();
+	}
+
+	public int getHeight() {
+		return bitmap.getHeight();
+	}
+
+	public float[] tofloatGreyArray() {
+		float[] rgbArray = tofloatRGBArray();
+		float[] res = new float[rgbArray.length / 3];
+		for (int i = 0; i < res.length; i++) {
+			int j = i * 3;
+			res[i] = (rgbArray[j] + rgbArray[j + 1] + rgbArray[j + 2]) / 3.f;
+		}
+		return res;
+	}
+}

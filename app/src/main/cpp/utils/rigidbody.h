90d673a942a2c76cbce73bf948bc968067b79074
Ipsedo <7268@samu>
2018-09-05 22:36:15 +0200

    suppr truc inutile

--- app/src/main/cpp/utils/rigidbody.h
+++ app/src/main/cpp/utils/rigidbody.h
@@ -19,37 +19,4 @@ localCreateInfo(btScalar mass, const btTransform &startTransform, btCollisionSha
 
 btConvexHullShape *parseObj(string objFileText);
 
-template<typename F>
-btRigidBody::btRigidBodyConstructionInfo
-makeCInfo(F fun, glm::vec3 pos, glm::vec3 scale, glm::mat4 rotMat, float mass) {
-	btCollisionShape *shape = fun();
-
-	btTransform myTransform;
-	myTransform.setIdentity();
-	myTransform.setOrigin(btVector3(pos.x, pos.y, pos.z));
-	glm::quat tmp = glm::quat_cast(rotMat);
-	myTransform.setRotation(btQuaternion(tmp.x, tmp.y, tmp.z, tmp.w));
-
-	return localCreateInfo(mass, myTransform, shape);
-};
-
-template<class C, typename FunShape, typename FunBase>
-C *makeBase(AAssetManager *mgr, FunShape f1, FunBase f2, glm::vec3 pos, glm::vec3 scale, glm::mat4 rotMat, float mass) {
-
-	btCollisionShape *shape = f1(scale);
-
-	btTransform myTransform;
-	myTransform.setIdentity();
-	myTransform.setOrigin(btVector3(pos.x, pos.y, pos.z));
-	glm::quat tmp = glm::quat_cast(rotMat);
-	myTransform.setRotation(btQuaternion(tmp.x, tmp.y, tmp.z, tmp.w));
-
-	btRigidBody::btRigidBodyConstructionInfo cinfo = localCreateInfo(mass, myTransform, shape);
-
-	return f2(mgr, cinfo, scale);
-};
-
-/*btRigidBody::btRigidBodyConstructionInfo
-createRBInfo(btCollisionShape shape, glm::vec3 pos, glm::vec3 scale, glm::mat4 rotMat)*/
-
 #endif //PHYVR_RIGODBODY_H

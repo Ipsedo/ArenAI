7157f0b16d53b772587b56724c0e7c653ba63905
Ipsedo <berriens@gmail.com>
2019-01-22 11:51:53 +0100

    updated shaders

--- app/src/main/assets/shaders/normal_map_fs.glsl
+++ app/src/main/assets/shaders/normal_map_fs.glsl
@@ -2,12 +2,11 @@
 precision mediump float;
 uniform sampler2D u_tex;
 uniform sampler2D u_normal_map;
+uniform vec3 u_light_pos;
 
 varying vec3 v_position;
 varying vec2 v_tex_coord;
 varying vec3 v_normal;
-varying vec3 v_light_dir;
-varying vec3 v_view_vec;
 
 // https://geeks3d.developpez.com/normal-mapping-glsl/
 mat3 cotangent_frame(vec3 N, vec3 p, vec2 uv) {
@@ -24,14 +23,15 @@ mat3 cotangent_frame(vec3 N, vec3 p, vec2 uv) {
     vec3 B = dp2perp * duv1.y + dp1perp * duv2.y;
 
     // construit une trame invariante a l'echelle
-    float invmax = inversesqrt( max( dot(T,T), dot(B,B) ) );
-    return mat3( T * invmax, B * invmax, N );
+    float invmax = inversesqrt(max(dot(T,T), dot(B,B)));
+    return mat3(T * invmax, B * invmax, N);
 }
 
 vec3 get_normal( vec3 N, vec3 V, vec2 texcoord ) {
     // N, la normale interpolee et
     // V, le vecteur vue (vertex dirige vers l'oeil)
-    vec3 map = normalize(2.0 * texture2D(u_normal_map, texcoord ).rgb - 1.0);
+    vec3 map = texture2D(u_normal_map, texcoord).rgb;
+    map = map * 255./127. - 128./127.;
     mat3 TBN = cotangent_frame(N, -V, texcoord);
     return normalize(TBN * map);
 }
@@ -40,11 +40,11 @@ void main() {
     vec2 uv = v_tex_coord;
 
     vec3 N = normalize(v_normal);
-    vec3 L = normalize(v_light_dir);
-    vec3 V = normalize(v_view_vec);
+    vec3 L = normalize(u_light_pos - v_position);
+    vec3 V = normalize(-v_position);
     vec3 PN = get_normal(N, V, uv);
 
-    float lambertTerm = max(dot(PN, L), 0.1);
+    float lambertTerm = max(dot(PN, L), 0.0);
     /*if (lambertTerm > 0.0) {
         final_color = lambertTerm * texColor;
 
--- app/src/main/assets/shaders/normal_map_vs.glsl
+++ app/src/main/assets/shaders/normal_map_vs.glsl
@@ -1,6 +1,5 @@
 uniform mat4 u_mvp_matrix;
 uniform mat4 u_mv_matrix;
-uniform vec3 u_light_pos;
 
 attribute vec3 a_position;
 attribute vec3 a_normal;
@@ -9,8 +8,6 @@ attribute vec2 a_tex_coord;
 varying vec3 v_position;
 varying vec2 v_tex_coord;
 varying vec3 v_normal;
-varying vec3 v_light_dir;
-varying vec3 v_view_vec;
 
 void main() {
     v_position = vec3(u_mv_matrix * vec4(a_position, 1.0));
@@ -18,9 +15,6 @@ void main() {
     v_tex_coord = a_tex_coord;
 
     v_normal = normalize(vec3(u_mv_matrix * vec4(a_normal, 0.0)));
-
-	v_view_vec = -v_position;
-	v_light_dir = u_light_pos - v_position;
 
     gl_Position = u_mvp_matrix * vec4(a_position, 1.0);
 }

da4791976f5664e41b10d44c36c7469f36701afe
Ipsedo <berriens@gmail.com>
2019-05-05 21:39:41 +0200

    suppr android makefile

--- old_unused/Application.mk
+++
@@ -1,7 +0,0 @@
-APP_MODULES := bullet phyvr libpng
-APP_ABI := all
-APP_OPTIM := release
-
-#We only need STL for placement new (#include <new>) 
-#We don't use STL in Bullet
-APP_STL                 := c++_shared

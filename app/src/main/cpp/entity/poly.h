8aed4cfe15fd15e956565fe0d0d1a189a60c1368
Ipsedo <berriens@gmail.com>
2019-12-21 22:47:53 +0100

    use std::function

--- app/src/main/cpp/entity/poly.h
+++ app/src/main/cpp/entity/poly.h
@@ -5,42 +5,22 @@
 #ifndef PHYVR_POLY_H
 #define PHYVR_POLY_H
 
-#include "../utils/assets.h"
-#include "../utils/rigidbody.h"
 #include "base.h"
 #include <glm/glm.hpp>
-#include <glm/gtc/quaternion.hpp>
+#include <android/asset_manager.h>
 
 class Poly : public Base {
 private:
-	template<typename FunShape>
-	static btRigidBodyConstructionInfo makeCInfo(FunShape makeShapFun, glm::vec3 pos,
-												 glm::mat4 rotMat, glm::vec3 scale, float mass) {
-		btCollisionShape *shape = makeShapFun(scale);
-
-		btTransform myTransform;
-		myTransform.setIdentity();
-		myTransform.setOrigin(btVector3(pos.x, pos.y, pos.z));
-		glm::quat tmp = glm::quat_cast(rotMat);
-		myTransform.setRotation(btQuaternion(tmp.x, tmp.y, tmp.z, tmp.w));
-
-		return localCreateInfo(mass, myTransform, shape);
-	};
+	static btRigidBodyConstructionInfo makeCInfo(
+			std::function<btCollisionShape *(glm::vec3)> makeShapeFun, glm::vec3 pos,
+			glm::mat4 rotMat, glm::vec3 scale, float mass);
 
 public:
-	template<typename FunShape>
-	Poly(FunShape makeShapFun, GLDrawable *modelVBO, const glm::vec3 pos,
-		 const glm::vec3 &scale, glm::mat4 rotMat, float mass, bool hasOwnModel) : Base(
-			makeCInfo(makeShapFun, pos, rotMat, scale, mass), modelVBO, scale, hasOwnModel) {}
+	Poly(std::function<btCollisionShape *(glm::vec3)> makeShapeFun,
+			GLDrawable *modelVBO, const glm::vec3 pos,
+			const glm::vec3 &scale, glm::mat4 rotMat, float mass, bool hasOwnModel);
 
-	static DiffuseModel *makeModel(AAssetManager *mgr, string objFileName) {
-		string objTxt = getFileText(mgr, objFileName);
-		return new ModelVBO(objTxt,
-							(float) rand() / RAND_MAX,
-							(float) rand() / RAND_MAX,
-							(float) rand() / RAND_MAX,
-							1.f);
-	};
+	static DiffuseModel *makeModel(AAssetManager *mgr, string objFileName);
 };
 
 #endif //PHYVR_POLY_H

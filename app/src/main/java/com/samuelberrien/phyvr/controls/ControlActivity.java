2f63285e4f0168ab3e6cca8c44dff1fb86523e04
Ipsedo <berriens@gmail.com>
2020-03-24 22:34:04 +0100

    changed text

--- app/src/main/java/com/samuelberrien/phyvr/controls/ControlActivity.java
+++ app/src/main/java/com/samuelberrien/phyvr/controls/ControlActivity.java
@@ -28,98 +28,82 @@ public class ControlActivity extends AppCompatActivity implements SharedPreferen
 
 		SeekBar sk_dir = findViewById(R.id.seekbar_dir);
 		TextView tv = findViewById(R.id.textview_dir);
-		tv.setText("Direction ratio : " + pref.getFloat(DirectionRatioKey, 1.f));
+		tv.setText("ratio : " + pref.getFloat(DirectionRatioKey, 1.f));
 		sk_dir.setProgress((int) (pref.getFloat(DirectionRatioKey, 1.f) * 100));
 		sk_dir.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 				float ratio = progress / 100.f;
 				TextView tv = findViewById(R.id.textview_dir);
-				tv.setText("Direction ratio : " + ratio);
+				tv.setText("ratio : " + ratio);
 				pref.edit().putFloat(DirectionRatioKey, ratio).apply();
 			}
 
 			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-
-			}
+			public void onStartTrackingTouch(SeekBar seekBar) {}
 
 			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-
-			}
+			public void onStopTrackingTouch(SeekBar seekBar) {}
 		});
 
 		SeekBar sk_turret = findViewById(R.id.seekbar_turret);
 		tv = findViewById(R.id.textview_turret);
-		tv.setText("Turret ratio : " + pref.getFloat(TurretRatioKey, 0.5f));
+		tv.setText("ratio : " + pref.getFloat(TurretRatioKey, 0.5f));
 		sk_turret.setProgress((int) (pref.getFloat(TurretRatioKey, 0.5f) * 100));
 		sk_turret.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 				float ratio = progress / 100.f;
 				TextView tv = findViewById(R.id.textview_turret);
-				tv.setText("Turret ratio : " + ratio);
+				tv.setText("ratio : " + ratio);
 				pref.edit().putFloat(TurretRatioKey, ratio).apply();
 			}
 
 			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-
-			}
+			public void onStartTrackingTouch(SeekBar seekBar) {}
 
 			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-
-			}
+			public void onStopTrackingTouch(SeekBar seekBar) {}
 		});
 
 		SeekBar sk_canon = findViewById(R.id.seekbar_canon);
 		tv = findViewById(R.id.textview_canon);
-		tv.setText("Canon ratio : " + pref.getFloat(CanonRatioKey, 0.5f));
+		tv.setText("ratio : " + pref.getFloat(CanonRatioKey, 0.5f));
 		sk_canon.setProgress((int) (pref.getFloat(CanonRatioKey, 0.5f) * 100));
 		sk_canon.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 				float ratio = progress / 100.f;
 				TextView tv = findViewById(R.id.textview_canon);
-				tv.setText("Canon ratio : " + ratio);
+				tv.setText("ratio : " + ratio);
 				pref.edit().putFloat(CanonRatioKey, ratio).apply();
 			}
 
 			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-
-			}
+			public void onStartTrackingTouch(SeekBar seekBar) {}
 
 			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-
-			}
+			public void onStopTrackingTouch(SeekBar seekBar) {}
 		});
 
 		SeekBar sk_speed = findViewById(R.id.seekbar_speed);
 		tv = findViewById(R.id.textview_speed);
-		tv.setText("Speed ratio : " + pref.getFloat(SpeedRatioKey, 1.f));
+		tv.setText("ratio : " + pref.getFloat(SpeedRatioKey, 1.f));
 		sk_speed.setProgress((int) (pref.getFloat(SpeedRatioKey, 1.f) * 100));
 		sk_speed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 				float ratio = progress / 100.f;
 				TextView tv = findViewById(R.id.textview_speed);
-				tv.setText("Speed ratio : " + ratio);
+				tv.setText("ratio : " + ratio);
 				pref.edit().putFloat(SpeedRatioKey, ratio).apply();
 			}
 
 			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-
-			}
+			public void onStartTrackingTouch(SeekBar seekBar) {}
 
 			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-
-			}
+			public void onStopTrackingTouch(SeekBar seekBar) {}
 		});
 	}
 

1583e62d904fd0e4d735d633cdcdbd3eec0a927b
samuel <berriens@gmail.com>
2025-10-09 21:11:54 +0200

    try use Vulkan but not working...

--- host/python/export_model_to_pte.py
+++ host/python/export_model_to_pte.py
@@ -7,39 +7,37 @@ from python.loader import load_neutral_state_into
 
 
 def main() -> None:
-    output_pte = "/home/samuel/Téléchargements/actor.pte"
-    state_dict_path = "/home/samuel/Téléchargements/actor_export"
-    nb_sensors = 3 * 3 + 3 * 3
-    nb_actions = 2 + 2 + 3
-
-    actor = SacActor(nb_sensors, nb_actions, 64, 256)
-
-    load_neutral_state_into(actor, state_dict_path)
-
-    batch = Dim("batch", min=1, max=32)
-    dynamic_shapes = (
-        {0: batch},
-        {0: batch},
-    )
-
-    example_input = (th.randn(2, 3, 128, 128), th.randn(2, nb_sensors))
-
-    exported_program = export(actor, example_input, dynamic_shapes=dynamic_shapes)
-
-    gm = exported_program.module()
     with th.no_grad():
-        print(gm(*example_input))
+        output_pte = "/home/samuel/Téléchargements/actor.pte"
+        state_dict_path = "/home/samuel/Téléchargements/actor_export"
+        nb_sensors = 3 * 3 + 3 * 3
+        nb_actions = 2 + 2 + 3
 
-    executorch_program = to_edge_transform_and_lower(
-        exported_program,
-        partitioner=[XnnpackPartitioner()],
-    ).to_executorch()
+        actor = SacActor(nb_sensors, nb_actions, 64, 256)
 
-    with open(output_pte, "wb") as file:
-        file.write(executorch_program.buffer)
+        load_neutral_state_into(actor, state_dict_path)
 
-    with th.no_grad():
-        print(actor(*example_input))
+        print("Model loaded from C++ !")
+
+        batch = Dim("batch", min=1, max=32)
+        dynamic_shapes = (
+            {0: batch},
+            {0: batch},
+        )
+
+        example_input = (th.randn(2, 3, 128, 128), th.randn(2, nb_sensors))
+
+        exported_program = export(actor, example_input, dynamic_shapes=dynamic_shapes)
+
+        executorch_program = to_edge_transform_and_lower(
+            exported_program,
+            partitioner=[XnnpackPartitioner()],
+        ).to_executorch()
+
+        with open(output_pte, "wb") as file:
+            file.write(executorch_program.buffer)
+
+        print("Model saved at", output_pte)
 
 
 if __name__ == '__main__':

8348469033030f18780e056a3154f236435631d0
Ipsedo <berriens@gmail.com>
2025-10-20 20:00:32 +0200

    variable naming + remove debugging checks

--- host/src/utils/torch_converter.cpp
+++ host/src/utils/torch_converter.cpp
@@ -38,40 +38,32 @@ std::tuple<torch::Tensor, torch::Tensor> states_to_tensor(const std::vector<Stat
     const torch::Tensor visions_u8 = torch::zeros(
         {N, C, H, W}, torch::TensorOptions().dtype(torch::kUInt8).requires_grad(false));
 
-    torch::Tensor proprio =
+    torch::Tensor proprioceptions =
         torch::zeros({N, P}, torch::TensorOptions().dtype(torch::kFloat).requires_grad(false));
 
-    auto *vis_ptr = visions_u8.data_ptr<uint8_t>();
-    auto *prop_ptr = proprio.data_ptr<float>();
+    auto *vision_ptr = visions_u8.data_ptr<uint8_t>();
+    auto *proprioception_ptr = proprioceptions.data_ptr<float>();
 
     constexpr size_t vision_stride = C * H * W;
-    constexpr auto row_bytes = static_cast<size_t>(W);
-    constexpr auto prop_bytes = static_cast<size_t>(P) * sizeof(float);
+    constexpr auto vision_row_bytes = static_cast<size_t>(W);
+    constexpr auto proprioception_bytes = static_cast<size_t>(P) * sizeof(float);
 
     at::parallel_for(0, N, 1, [&](const int64_t begin, const int64_t end) {
         for (int64_t n = begin; n < end; ++n) {
-            if (states[n].vision.size() != C)
-                throw std::invalid_argument("states[n].vision.size() != C");
-
-            uint8_t *dst = vis_ptr + n * vision_stride;
+            uint8_t *vision_dst = vision_ptr + n * vision_stride;
             for (int64_t c = 0; c < C; ++c) {
-                if (states[n].vision[c].size() != H)
-                    throw std::invalid_argument("states[n].vision[c].size() != H");
-
                 for (int64_t h = 0; h < H; ++h) {
-                    if (states[n].vision[c][h].size() != W)
-                        throw std::invalid_argument("states[n].vision[c][h].size() != W");
-
-                    const uint8_t *src_row = states[n].vision[c][h].data();
-                    std::memcpy(dst, src_row, row_bytes);
-                    dst += static_cast<size_t>(W);
+                    const uint8_t *vision_src_row = states[n].vision[c][h].data();
+                    std::memcpy(vision_dst, vision_src_row, vision_row_bytes);
+                    vision_dst += static_cast<size_t>(W);
                 }
             }
-            std::memcpy(prop_ptr + n * P, states[n].proprioception.data(), prop_bytes);
+            std::memcpy(
+                proprioception_ptr + n * P, states[n].proprioception.data(), proprioception_bytes);
         }
     });
 
-    return {visions_u8, proprio};
+    return {visions_u8, proprioceptions};
 }
 
 std::tuple<torch::Tensor, torch::Tensor> state_to_tensor(const State &state) {
@@ -80,33 +72,26 @@ std::tuple<torch::Tensor, torch::Tensor> state_to_tensor(const State &state) {
     constexpr int64_t W = ENEMY_VISION_SIZE;
     constexpr int64_t P = ENEMY_PROPRIOCEPTION_SIZE;
 
-    const torch::Tensor visions_u8 =
+    const torch::Tensor vision_u8 =
         torch::zeros({C, H, W}, torch::TensorOptions().dtype(torch::kUInt8).requires_grad(false));
 
-    torch::Tensor proprio =
+    torch::Tensor proprioception =
         torch::zeros({P}, torch::TensorOptions().dtype(torch::kFloat).requires_grad(false));
 
-    auto *vis_ptr = visions_u8.data_ptr<uint8_t>();
-    auto *prop_ptr = proprio.data_ptr<float>();
+    auto *vision_ptr = vision_u8.data_ptr<uint8_t>();
+    auto *proprioception_ptr = proprioception.data_ptr<float>();
 
-    constexpr auto row_bytes = static_cast<size_t>(W);
-
-    if (state.vision.size() != C) throw std::invalid_argument("states[n].vision.size() != C");
+    constexpr auto vision_row_bytes = static_cast<size_t>(W);
 
     for (int64_t c = 0; c < C; ++c) {
-        if (state.vision[c].size() != H)
-            throw std::invalid_argument("states[n].vision[c].size() != H");
-
         for (int64_t h = 0; h < H; ++h) {
-            if (state.vision[c][h].size() != W)
-                throw std::invalid_argument("states[n].vision[c][h].size() != W");
-
-            const uint8_t *src_row = state.vision[c][h].data();
-            std::memcpy(vis_ptr, src_row, row_bytes);
-            vis_ptr += static_cast<size_t>(W);
+            const uint8_t *vision_src_row = state.vision[c][h].data();
+            std::memcpy(vision_ptr, vision_src_row, vision_row_bytes);
+            vision_ptr += static_cast<size_t>(W);
         }
     }
-    std::memcpy(prop_ptr, state.proprioception.data(), static_cast<size_t>(P) * sizeof(float));
+    std::memcpy(
+        proprioception_ptr, state.proprioception.data(), static_cast<size_t>(P) * sizeof(float));
 
-    return {visions_u8, proprio};
+    return {vision_u8, proprioception};
 }

b84e8f4e676915c6f3dbfbd13fba268fd7f826f0
Ipsedo <berriens@gmail.com>
2025-10-23 23:05:22 +0200

    update hyper params : learn 2 times per step

--- arenai/arenai_train/src/main.cpp
+++ arenai/arenai_train/src/main.cpp
@@ -14,24 +14,24 @@ int main(int argc, char **argv) {
     parser.add_argument("--hidden_size_sensors").scan<'i', int>().default_value(192);
     parser.add_argument("--hidden_size_actions").scan<'i', int>().default_value(64);
     parser.add_argument("--hidden_size").scan<'i', int>().default_value(1024);
-    parser.add_argument("--tau").scan<'g', float>().default_value(1e-2f);
+    parser.add_argument("--tau").scan<'g', float>().default_value(0.005f);
     parser.add_argument("--gamma").scan<'g', float>().default_value(0.99f);
     parser.add_argument("--initial_alpha").scan<'g', float>().default_value(1.f);
 
     // train
-    parser.add_argument("--nb_tanks").scan<'i', int>().default_value(16);
+    parser.add_argument("--nb_tanks").scan<'i', int>().default_value(8);
     parser.add_argument("--output_folder").required();
     parser.add_argument("--asset_folder").required();
-    parser.add_argument("--learning_rate").scan<'g', float>().default_value(1e-4f);
-    parser.add_argument("--epochs").scan<'i', int>().default_value(8);
-    parser.add_argument("--batch_size").scan<'i', int>().default_value(2048);
+    parser.add_argument("--learning_rate").scan<'g', float>().default_value(3e-4f);
+    parser.add_argument("--epochs").scan<'i', int>().default_value(10);
+    parser.add_argument("--batch_size").scan<'i', int>().default_value(400);
     parser.add_argument("--max_episode_steps").scan<'i', int>().default_value(30 * 60 * 3);
-    parser.add_argument("--nb_episodes").scan<'i', int>().default_value(32768);
-    parser.add_argument("--replay_buffer_size").scan<'i', int>().default_value(131072);
-    parser.add_argument("--train_every").scan<'i', int>().default_value(256);
+    parser.add_argument("--nb_episodes").scan<'i', int>().default_value(30000);
+    parser.add_argument("--replay_buffer_size").scan<'i', int>().default_value(200000);
+    parser.add_argument("--train_every").scan<'i', int>().default_value(250);
     parser.add_argument("--save_every").scan<'i', int>().default_value(30 * 60 * 3 * 25);
     parser.add_argument("--cuda").default_value(false).implicit_value(true);
-    parser.add_argument("--metric_window_size").scan<'i', int>().default_value(256);
+    parser.add_argument("--metric_window_size").scan<'i', int>().default_value(1000);
 
     parser.parse_args(argc, argv);
 

03afef4809589c5f7450d810f16922bbf92c76b4
samuel <berriens@gmail.com>
2025-11-01 01:40:58 +0100

    scale potential reward

--- arenai/arenai_train/src/main.cpp
+++ arenai/arenai_train/src/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv) {
     parser.add_argument("--nb_tanks").scan<'i', int>().default_value(8);
     parser.add_argument("--output_folder").required();
     parser.add_argument("--asset_folder").required();
-    parser.add_argument("--potential_reward_factor").scan<'g', float>().default_value(1e2f);
+    parser.add_argument("--potential_reward_scale").scan<'g', float>().default_value(1e2f);
     parser.add_argument("--learning_rate").scan<'g', float>().default_value(3e-4f);
     parser.add_argument("--epochs").scan<'i', int>().default_value(5);
     parser.add_argument("--batch_size").scan<'i', int>().default_value(800);
@@ -43,7 +43,7 @@ int main(int argc, char **argv) {
         {parser.get<int>("--nb_tanks"),
          std::filesystem::path(parser.get<std::string>("--output_folder")),
          std::filesystem::path(parser.get<std::string>("--asset_folder")),
-         parser.get<float>("--potential_reward_factor"), parser.get<float>("--learning_rate"),
+         parser.get<float>("--potential_reward_scale"), parser.get<float>("--learning_rate"),
          parser.get<int>("--epochs"), parser.get<int>("--batch_size"),
          parser.get<int>("--max_episode_steps"), parser.get<int>("--nb_episodes"),
          parser.get<int>("--replay_buffer_size"), parser.get<int>("--train_every"),
--- arenai/arenai_train/src/train.h
+++ arenai/arenai_train/src/train.h
@@ -20,7 +20,7 @@ struct TrainOptions {
     int nb_tanks;
     std::filesystem::path output_folder;
     std::filesystem::path android_asset_folder;
-    float potential_reward_factor;
+    float potential_reward_scale;
     float learning_rate;
     int epochs;
     int batch_size;

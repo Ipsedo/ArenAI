ab942e0b54b518e6a71710ffa1ac5566bd910ac0
Ipsedo <berriens@gmail.com>
2026-03-22 23:26:29 +0100

    init field

--- arenai/arenai_core/src/enemy_tank_factory.cpp
+++ arenai/arenai_core/src/enemy_tank_factory.cpp
@@ -17,8 +17,8 @@ EnemyTankFactory::EnemyTankFactory(
       tank_prefix_name(tank_prefix_name), hit_reward(0.f),
       max_frames_upside_down(static_cast<int>(4.f / wanted_frame_frequency)),
       curr_frame_upside_down(0), is_dead_already_triggered(false),
-      sigma_angle(static_cast<float>(M_PI) / 4.f), softmax_beta(4.f), has_touch(false),
-      action_stats(std::make_shared<ActionStats>()) {}
+      sigma_angle(static_cast<float>(M_PI) / 4.f), sigma_distance(200.f), optimal_distance(100.f),
+      softmax_beta(4.f), has_touch(false), action_stats(std::make_shared<ActionStats>()) {}
 
 float EnemyTankFactory::compute_aim_angle(const std::unique_ptr<EnemyTankFactory> &other_tank) {
     const auto canon_tr = get_canon()->get_model_matrix();

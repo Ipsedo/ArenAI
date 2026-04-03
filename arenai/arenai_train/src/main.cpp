d0ec7ef18629d82e61ec5a6ba7230f0b92e746da
Ipsedo <berriens@gmail.com>
2026-04-03 12:12:45 +0200

    larger space and more tank (but with less density)

--- arenai/arenai_train/src/main.cpp
+++ arenai/arenai_train/src/main.cpp
@@ -31,13 +31,13 @@ int main(const int argc, char **argv) {
     parser.add_argument("--initial_alpha").scan<'g', float>().default_value(1.f);
 
     // train
-    parser.add_argument("--nb_tanks").scan<'i', int>().default_value(16);
+    parser.add_argument("--nb_tanks").scan<'i', int>().default_value(32);
     parser.add_argument("--output_folder").required();
     parser.add_argument("--asset_folder").required();
     parser.add_argument("--learning_rate").scan<'g', float>().default_value(3e-4f);
     parser.add_argument("--potential_reward_scale").scan<'g', float>().default_value(1.f);
     parser.add_argument("--epochs").scan<'i', int>().default_value(16);
-    parser.add_argument("--batch_size").scan<'i', int>().default_value(512);
+    parser.add_argument("--batch_size").scan<'i', int>().default_value(2048);
     parser.add_argument("--max_episode_steps").scan<'i', int>().default_value(30 * 60 * 3);
     parser.add_argument("--nb_episodes").scan<'i', int>().default_value(10000);
     parser.add_argument("--replay_buffer_size").scan<'i', int>().default_value(100000);

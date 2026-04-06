38ea76065b67037545f8b1a9f4e1ba3f908c4db3
Ipsedo <berriens@gmail.com>
2026-04-06 03:45:21 +0200

    fix init

--- arenai/arenai_train/src/networks/init.cpp
+++ arenai/arenai_train/src/networks/init.cpp
@@ -5,25 +5,20 @@
 #include "./init.h"
 
 void init_weights(torch::nn::Module &module) {
-    if (typeid(module) == typeid(torch::nn::Linear)) {
-        auto lin = *dynamic_cast<torch::nn::Linear *>(&module);
-
+    if (auto *lin = dynamic_cast<torch::nn::LinearImpl *>(&module)) {
         torch::nn::init::kaiming_normal_(lin->weight);
         if (lin->options.bias()) torch::nn::init::zeros_(lin->bias);
-    } else if (typeid(module) == typeid(torch::nn::LayerNorm)) {
-        if (auto ln = *dynamic_cast<torch::nn::LayerNorm *>(&module);
-            ln->options.elementwise_affine()) {
+    } else if (auto *ln = dynamic_cast<torch::nn::LayerNormImpl *>(&module)) {
+        if (ln->options.elementwise_affine()) {
             torch::nn::init::ones_(ln->weight);
             torch::nn::init::zeros_(ln->bias);
         }
-    } else if (typeid(module) == typeid(torch::nn::GroupNorm)) {
-        if (auto gn = *dynamic_cast<torch::nn::GroupNorm *>(&module); gn->options.affine()) {
+    } else if (auto *gn = dynamic_cast<torch::nn::GroupNormImpl *>(&module)) {
+        if (gn->options.affine()) {
             torch::nn::init::ones_(gn->weight);
             torch::nn::init::zeros_(gn->bias);
         }
-    } else if (typeid(module) == typeid(torch::nn::Conv2d)) {
-        auto conv = *dynamic_cast<torch::nn::Conv2d *>(&module);
-
+    } else if (auto *conv = dynamic_cast<torch::nn::Conv2dImpl *>(&module)) {
         torch::nn::init::kaiming_normal_(conv->weight);
         if (conv->options.bias()) torch::nn::init::zeros_(conv->bias);
     }

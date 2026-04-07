f9f5f939d596bf3ce4ad9b6db3f67bd028662463
samuel <berriens@gmail.com>
2026-04-07 21:31:39 +0200

    gaussian tanh + more dense tank sampling

--- arenai/arenai_train/src/main.cpp
+++ arenai/arenai_train/src/main.cpp
@@ -28,14 +28,14 @@ int main(const int argc, char **argv) {
     parser.add_argument("--critic_hidden_size").scan<'i', int>().default_value(384);
     parser.add_argument("--tau").scan<'g', float>().default_value(0.005f);
     parser.add_argument("--gamma").scan<'g', float>().default_value(0.99f);
-    parser.add_argument("--initial_alpha").scan<'g', float>().default_value(1.f);
+    parser.add_argument("--initial_alpha").scan<'g', float>().default_value(0.01f);
 
     // train
-    parser.add_argument("--nb_tanks").scan<'i', int>().default_value(32);
+    parser.add_argument("--nb_tanks").scan<'i', int>().default_value(16);
     parser.add_argument("--output_folder").required();
     parser.add_argument("--asset_folder").required();
     parser.add_argument("--learning_rate").scan<'g', float>().default_value(3e-4f);
-    parser.add_argument("--alpha_learning_rate").scan<'g', float>().default_value(1e-5f);
+    parser.add_argument("--alpha_learning_rate").scan<'g', float>().default_value(1e-4f);
     parser.add_argument("--potential_reward_scale").scan<'g', float>().default_value(1.f);
     parser.add_argument("--epochs").scan<'i', int>().default_value(16);
     parser.add_argument("--batch_size").scan<'i', int>().default_value(1024);

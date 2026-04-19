5c848ef9c80aee062db7ef2a0c1cd6bef7d98530
Ipsedo <berriens@gmail.com>
2026-04-19 22:36:26 +0200

    lower decoder loss factor

--- arenai/arenai_train/src/agents/sac.cpp
+++ arenai/arenai_train/src/agents/sac.cpp
@@ -58,7 +58,7 @@ SacAgent::SacAgent(
       gamma(gamma),
       continous_target_entropy(truncated_normal_target_entropy(nb_continuous_actions, 0.2f)),
       discrete_target_entropy(multinomial_target_entropy(nb_discrete_actions, 0.98f)),
-      decoding_loss_factor(0.5f) {
+      decoding_loss_factor(0.25f) {
 
     hard_update(target_critic_1, critic_1);
     hard_update(target_critic_2, critic_2);

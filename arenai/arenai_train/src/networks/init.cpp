ff41af870d79a71972dcabf6550836e922a903ee
samuel <berriens@gmail.com>
2026-06-06 04:17:44 +0200

    sigma target entropy to 0.1 => kill critic => backtrack to 0.5

--- arenai/arenai_train/src/networks/init.cpp
+++ arenai/arenai_train/src/networks/init.cpp
@@ -26,7 +26,7 @@ void init_hidden_weights(torch::nn::Module &module) {
 
 void init_mu_output_weights(torch::nn::Module &module) {
     if (auto *lin = dynamic_cast<torch::nn::LinearImpl *>(&module)) {
-        torch::nn::init::uniform_(lin->weight, -0.5f, 0.5f);
+        torch::nn::init::uniform_(lin->weight, -1e-3f, 1e-3f);
         if (lin->options.bias()) torch::nn::init::zeros_(lin->bias);
     }
 }
@@ -40,14 +40,14 @@ void init_sigma_output_weights(torch::nn::Module &module) {
 
 void init_discrete_output_weights(torch::nn::Module &module) {
     if (auto *lin = dynamic_cast<torch::nn::LinearImpl *>(&module)) {
-        torch::nn::init::uniform_(lin->weight, -0.5f, 0.5f);
+        torch::nn::init::uniform_(lin->weight, -0.1f, 0.1f);
         if (lin->options.bias()) torch::nn::init::zeros_(lin->bias);
     }
 }
 
 void init_value_output_weights(torch::nn::Module &module) {
     if (auto *lin = dynamic_cast<torch::nn::LinearImpl *>(&module)) {
-        torch::nn::init::uniform_(lin->weight, -0.1f, 0.1f);
+        torch::nn::init::uniform_(lin->weight, -0.5f, 0.5f);
         if (lin->options.bias()) torch::nn::init::zeros_(lin->bias);
     }
 }

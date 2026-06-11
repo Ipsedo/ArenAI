321298bece972f2e3906c72377ff17ea17d9da98
Ipsedo <berriens@gmail.com>
2026-06-11 22:42:06 +0200

    higher target discrete entropy

--- arenai/arenai_train/src/agents/sac.cpp
+++ arenai/arenai_train/src/agents/sac.cpp
@@ -62,7 +62,7 @@ SacAgent::SacAgent(
       alpha_discrete_metric(std::make_shared<MeanMetric>("α_c", metric_window_size)), tau(tau),
       gamma(gamma),
       continuous_target_entropy(truncated_normal_target_entropy(nb_continuous_actions, 0.5f)),
-      discrete_target_entropy(0.5f * multinomial_maximum_entropy(nb_discrete_actions)) {
+      discrete_target_entropy(0.8f * multinomial_maximum_entropy(nb_discrete_actions)) {
 
     hard_update(target_critic_1, critic_1);
     hard_update(target_critic_2, critic_2);

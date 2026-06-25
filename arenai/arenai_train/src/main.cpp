dbb5d08a377cb905b19e82b6eacc2c4f6e1a61ec
Ipsedo <berriens@gmail.com>
2026-06-25 18:32:19 +0200

    set potential reward scale to 1.0

--- arenai/arenai_train/src/main.cpp
+++ arenai/arenai_train/src/main.cpp
@@ -39,7 +39,7 @@ int main(const int argc, char **argv) {
     parser.add_argument("--actor_learning_rate").scan<'g', float>().default_value(1e-4f);
     parser.add_argument("--critic_learning_rate").scan<'g', float>().default_value(3e-4f);
     parser.add_argument("--alpha_learning_rate").scan<'g', float>().default_value(1e-4f);
-    parser.add_argument("--potential_reward_scale").scan<'g', float>().default_value(0.1f);
+    parser.add_argument("--potential_reward_scale").scan<'g', float>().default_value(1.f);
     parser.add_argument("--target_continuous_sigma").scan<'g', float>().default_value(0.1f);
     parser.add_argument("--discrete_entropy_factor").scan<'g', float>().default_value(0.5f);
     parser.add_argument("--epochs").scan<'i', int>().default_value(4);

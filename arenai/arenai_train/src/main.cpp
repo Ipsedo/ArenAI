c02c444d14a63a374efec6c6105bbbc2be76a6ee
samuel <berriens@gmail.com>
2026-07-14 16:09:56 +0200

    2 times less tank density => lower train / sample ratio (1:1) higher learning rate, fewer channels in first conv

--- arenai/arenai_train/src/main.cpp
+++ arenai/arenai_train/src/main.cpp
@@ -19,10 +19,10 @@ int main(const int argc, char **argv) {
     // model
     parser.add_argument("--vision_channels")
         .default_value<vision_channels>(
-            {{{3, 32}, {32, 64}, {64, 128}, {128, 256}, {256, 256}, {256, 256}}})
+            {{{3, 8}, {8, 16}, {16, 32}, {32, 64}, {64, 128}, {128, 256}}})
         .action(parse_cli_vision_channels);
     parser.add_argument("--group_norm_nums")
-        .default_value<group_norm_nums>({{4, 8, 16, 32, 32, 32}})
+        .default_value<group_norm_nums>({{1, 2, 4, 8, 16, 32}})
         .action(parse_cli_group_norms);
     parser.add_argument("--sensors_hidden_size").scan<'i', int>().default_value(256);
     parser.add_argument("--actions_hidden_size").scan<'i', int>().default_value(64);
@@ -33,19 +33,19 @@ int main(const int argc, char **argv) {
         .default_value<hidden_layers>({{2560, 1280}})
         .action(parse_cli_hidden_layer);
     parser.add_argument("--tau").scan<'g', float>().default_value(0.005f);
-    parser.add_argument("--gamma").scan<'g', float>().default_value(0.99f);
+    parser.add_argument("--gamma").scan<'g', float>().default_value(0.995f);
 
     // train
     parser.add_argument("--output_folder").required();
     parser.add_argument("--resources_folder").required();
-    parser.add_argument("--actor_learning_rate").scan<'g', float>().default_value(1e-4f);
-    parser.add_argument("--critic_learning_rate").scan<'g', float>().default_value(3e-4f);
-    parser.add_argument("--alpha_learning_rate").scan<'g', float>().default_value(3e-4f);
+    parser.add_argument("--actor_learning_rate").scan<'g', float>().default_value(1e-3f);
+    parser.add_argument("--critic_learning_rate").scan<'g', float>().default_value(3e-3f);
+    parser.add_argument("--alpha_learning_rate").scan<'g', float>().default_value(3e-3f);
     parser.add_argument("--epochs").scan<'i', int>().default_value(8);
     parser.add_argument("--batch_size").scan<'i', int>().default_value(256);
     parser.add_argument("--max_episode_steps").scan<'i', int>().default_value(30 * 60 * 3);
     parser.add_argument("--nb_episodes").scan<'i', int>().default_value(5000);
-    parser.add_argument("--replay_buffer_size").scan<'i', int>().default_value(150000);
+    parser.add_argument("--replay_buffer_size").scan<'i', int>().default_value(200000);
     parser.add_argument("--train_every").scan<'i', int>().default_value(64);
     parser.add_argument("--save_every").scan<'i', int>().default_value(30 * 60 * 3 * 25);
     parser.add_argument("--cuda").default_value(false).implicit_value(true);
@@ -53,13 +53,13 @@ int main(const int argc, char **argv) {
 
     // env
     parser.add_argument("--wanted_frequency").scan<'g', float>().default_value(1.f / 30.f);
-    parser.add_argument("--nb_tanks").scan<'i', int>().default_value(16);
+    parser.add_argument("--nb_tanks").scan<'i', int>().default_value(32);
     parser.add_argument("--vision_height").scan<'i', int>().default_value(128);
     parser.add_argument("--vision_width").scan<'i', int>().default_value(256);
-    parser.add_argument("--initial_spawn_width").scan<'g', float>().default_value(250.f);
-    parser.add_argument("--initial_spawn_height").scan<'g', float>().default_value(250.f);
-    parser.add_argument("--final_spawn_width").scan<'g', float>().default_value(1000.f);
-    parser.add_argument("--final_spawn_height").scan<'g', float>().default_value(1000.f);
+    parser.add_argument("--initial_spawn_width").scan<'g', float>().default_value(500.f);
+    parser.add_argument("--initial_spawn_height").scan<'g', float>().default_value(500.f);
+    parser.add_argument("--final_spawn_width").scan<'g', float>().default_value(2000.f);
+    parser.add_argument("--final_spawn_height").scan<'g', float>().default_value(2000.f);
     parser.add_argument("--vision_num_threads")
         .scan<'i', int>()
         .default_value(static_cast<int>(std::thread::hardware_concurrency()));

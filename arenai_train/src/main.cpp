a955f96c8d1ca637ec27cd0779cff1918d3ffa9f
Ipsedo <berriens@gmail.com>
2026-07-17 11:04:31 +0200

    * cli arg groups * update README.md

--- arenai_train/src/main.cpp
+++ arenai_train/src/main.cpp
@@ -17,6 +17,7 @@ int main(const int argc, char **argv) {
     argparse::ArgumentParser parser("arenai train");
 
     // model
+    parser.add_group("model");
     parser.add_argument("--vision_channels")
         .default_value<vision_channels>(
             {{{3, 8}, {8, 16}, {16, 32}, {32, 64}, {64, 128}, {128, 256}}})
@@ -36,6 +37,7 @@ int main(const int argc, char **argv) {
     parser.add_argument("--gamma").scan<'g', float>().default_value(0.995f);
 
     // train
+    parser.add_group("train");
     parser.add_argument("--output_folder").required();
     parser.add_argument("--resources_folder").required();
     parser.add_argument("--actor_learning_rate").scan<'g', float>().default_value(1e-4f);
@@ -52,6 +54,7 @@ int main(const int argc, char **argv) {
     parser.add_argument("--metric_window_size").scan<'i', int>().default_value(256);
 
     // env
+    parser.add_group("environment");
     parser.add_argument("--wanted_frequency").scan<'g', float>().default_value(1.f / 30.f);
     parser.add_argument("--nb_tanks").scan<'i', int>().default_value(32);
     parser.add_argument("--vision_height").scan<'i', int>().default_value(128);

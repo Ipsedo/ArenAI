991c211fa412e7dba081c8169f2a80959bc8141b
Ipsedo <berriens@gmail.com>
2026-07-25 00:11:53 +0200

    update ppo hyper-params : * gamma from 0.995 to 0.99 * rollout from 10s to 30s * max grad norm from 1.0 to 0.6

--- arenai_agent/src/agents/ppo/ppo_hyperparams.h
+++ arenai_agent/src/agents/ppo/ppo_hyperparams.h
@@ -23,14 +23,14 @@ namespace arenai::agent {
                                                              {32, 64}, {64, 128}, {128, 256}};
         std::vector<int> group_norm_nums = {1, 2, 4, 8, 16, 32};
         int metric_window_size = 256;
-        float gamma = 0.995f;
+        float gamma = 0.99f;
         float gae_lambda = 0.95f;
         float clip_epsilon = 0.2f;
-        float grad_norm_max = 1.f;
+        float grad_norm_max = 0.5f;
         float continuous_entropy_coef = 0.005f;
         float discrete_entropy_coef = 0.01f;
         int epochs = 4;
-        int rollout_size = 300;// 10 s of game time at 30 Hz
+        int rollout_size = 30 * 30;
         int minibatch_size = 512;
     };
 

f94e10998fad60b1c0cd620ee05f2ca6f87253f6
Ipsedo <berriens@gmail.com>
2026-08-29 02:50:58 +0200

    start continuous target entropy at 0.5 instead of 0.25 + init sigma

--- arenai_agent/src/agents/ppo/ppo_hyperparams.h
+++ arenai_agent/src/agents/ppo/ppo_hyperparams.h
@@ -22,7 +22,7 @@ namespace arenai::agent {
         std::vector<std::tuple<int, int>> vision_channels = {{3, 8},   {8, 16},  {16, 24},
                                                              {24, 32}, {32, 48}, {48, 64}};
         std::vector<int> group_norm_nums = {1, 2, 3, 4, 6, 8};
-        float initial_sigma = 0.3f;
+        float initial_sigma = 0.5f;
         float initial_fire_proba = 0.5f;
         int metric_window_size = 256;
         float gamma = 0.99f;
